25f4bd9301c2340715f03f1d84b2b9f8821090ef
dikshabajaj <bajaj.diksha01@gmail.com>
2016-05-30 19:16:24 +0530

    Added

---
+++ teamnotfound_Windows_CS/teamnotfound/DataModel/Country.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teamnotfound.DataModel
+{
+    class Country
+    {
+        public string Id { get; set; }
+        [JsonProperty(PropertyName = "country_name")]
+        public string CountryName { set; get; }
+    }
+}
